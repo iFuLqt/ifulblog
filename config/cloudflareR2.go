a2e8c01c81e16422b3f2daaabfe978d894cc2746
Saiful Anwar <ifulqt@gmail.com>
2025-10-31 21:39:16 +0800

    Membuat config cloudflareR2

---
+++ config/cloudflareR2.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"context"
+	"github.com/rs/zerolog/log"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsConfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+)
+
+func (cfg Config) LoadAwsConfig() aws.Config {
+	conf, err := awsConfig.LoadDefaultConfig(context.TODO(),
+		awsConfig.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+			cfg.R2.ApiKey, cfg.R2.ApiSecret, "",
+		)), awsConfig.WithRegion("auto"),)
+	if err != nil {
+		log.Fatal().Msgf("unable to load AWS config %v", err)
+	}
+	log.Info().Msg("Success loaded AWS config")
+	return conf
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,14 +2,20 @@ package app
 
 import (
 	"ifulblog/config"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/rs/zerolog/log"
 )
 
 func RunServer() {
 	cfg := config.NewConfig()
 	_, err := cfg.ConnectionPostgres()
-	if err != nil{
+	if err != nil {
 		log.Fatal().Msgf("Error connecting to database %v", err)
 		return
 	}
+
+	//cloudfareR2
+	cdfR2 := cfg.LoadAwsConfig()
+	_ = s3.NewFromConfig(cdfR2)
 }
